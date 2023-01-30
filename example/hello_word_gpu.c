41af688fc772229651f4b61e6c9831586de38086
Henric Zazzi <hzazzi@kth.se>
2023-01-30 15:07:49 +0100

    added a hello world example for GPUs

---
+++ example/hello_word_gpu.c
@@ -0,0 +1,48 @@
+#include <hip/hip_runtime.h>
+#include <stdio.h>
+
+// Kernel function that copies data string 1 -> string 2
+__global__ void MyKernel(char* s1, char* s2) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  s2[idx] = s1[idx];
+  }
+
+int main(int argc, char** argv) {
+  char hosts1[] = "hello world";
+  char hosts2[11], *devs1, *devs2;
+  size_t size = sizeof(hosts1);
+  int ndevices, ndevice;
+
+  // Get number of GPUs available
+  if (hipGetDeviceCount(&ndevices) != hipSuccess) {
+    printf("No such devices\n");
+    return 1;
+    }
+  printf("You can access GPU devices: 0-%d\n", (ndevices - 1));
+  ndevice = 0;
+  if (argc > 1)
+    ndevice = atoi(argv[1]);
+  // Set default device to be used for subsequent hip API calls from this thread
+  if (hipSetDevice(ndevice) != hipSuccess) {
+    printf("Error initializing device %d\n", ndevice);
+    return 1;
+    }
+  // Allocate memory on device
+  hipMalloc(&devs1, size);
+  hipMalloc(&devs2, size);
+  // Copy data host -> device
+  hipMemcpy(devs1, hosts1, size, hipMemcpyHostToDevice);
+  // 3D-grid dimensions specifying the number of blocks to launch
+  dim3 ngrid(1);
+  // 3D-block dimensions specifying the number of threads in each block
+  dim3 nblock(size);
+  // Run kernel
+  hipLaunchKernelGGL(MyKernel, ngrid, nblock, 0, 0, devs1, devs2);
+  // Copy data device -> host
+  hipMemcpy(hosts2, devs2, size, hipMemcpyDeviceToHost);
+  // Free up memory
+  hipFree(devs1);
+  hipFree(devs2);
+  // print the results
+  printf("GPU %d: %s\n", ndevice, hosts2);
+  }
